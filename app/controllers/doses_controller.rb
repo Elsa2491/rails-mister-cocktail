46f498c0d016f30101a6842fa4149640e588b949
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2020-11-15 16:11:25 +0100

    mister cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+    # METHODE NEW
+    # Trouver dans quel cocktail je mets une new dose
+    # Pour ca, voir la route doses new : get/cocktails/:cocktail_id/doses/new
+    # Creer une nouvelle dose
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+    # METHOD CREATE
+    # Recuperer l'id du cocktail
+    # Creer une dose avec les strong_params
+    # Associer cette dose a un cocktail
+    # Save la dose
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.create(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.delete
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Lire la page d'acceuil
+  # Lire tous les cocktails
+  # Lire un cocktail
+  # Creer un cocktail (2 requetes)
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
